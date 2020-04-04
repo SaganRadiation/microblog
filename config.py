f10abaf2698301b24cb4c79a51cdccfbbe84f571
saganradiation <jakegrimes9@gmail.com>
2020-04-04 12:06:26 -0400

    Add posting capability

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['jakegrimes9@gmail.com']
-
+	POSTS_PER_PAGE = 25
